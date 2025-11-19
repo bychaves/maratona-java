8046d5f04c70e85288dc0010b7b4a8851e7f2e63
Gabriel Chaves <chaves_br@hotmail.com>
2025-11-18 22:03:53 -0300

    feature: Sobrescrita

---
+++ src/com/bychaves/maratonajava/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package com.bychaves.maratonajava.javacore.Isobrescrita.test;
+
+import com.bychaves.maratonajava.javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Vegeta");
+
+        System.out.println(anime);
+    }
+}
