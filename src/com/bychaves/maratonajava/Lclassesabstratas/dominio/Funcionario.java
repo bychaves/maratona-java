c36dcb309388bfe047fe0970db00f0f3e6f153ab
Gabriel Chaves <chaves_br@hotmail.com>
2025-11-22 17:54:39 -0300

    feature: Classes Abstratas

---
+++ src/com/bychaves/maratonajava/Lclassesabstratas/dominio/Funcionario.java
@@ -0,0 +1,28 @@
+package com.bychaves.maratonajava.Lclassesabstratas.dominio;
+
+public abstract class Funcionario extends Pessoa {
+    protected String nome;
+    protected double salario;
+
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+        calculaBonus();
+    }
+
+    public abstract void calculaBonus();
+
+    @Override
+    public void imprime() {
+        System.out.println("Imprime...");
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "nome='" + nome + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
+}
+
---
+++ src/com/bychaves/maratonajava/Lclassesabstratas/dominio/Pessoa.java
@@ -0,0 +1,7 @@
+package com.bychaves.maratonajava.Lclassesabstratas.dominio;
+
+public abstract class Pessoa {
+    public abstract void imprime();
+
+}
+
