4cf6b09b9dd6d7824bd8613b8f12e6ce7a26d177
Gabriel Chaves <chaves_br@hotmail.com>
2025-11-24 23:32:51 -0300

    feature: Exception

---
+++ src/com/bychaves/maratonajava/javacore/Oexception/error/test/StackOverFlowTest01.java
@@ -0,0 +1,11 @@
+package com.bychaves.maratonajava.javacore.Oexception.error.test;
+
+public class StackOverFlowTest01 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void recursividade(){
+        recursividade();
+    }
+}
